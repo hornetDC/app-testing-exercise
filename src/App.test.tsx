5ad767a5d06e1513e6826663e367beff09b8dbd6
Dmytro Murashko <dmytro.murashko.ext@extendaretail.com>
2022-04-22 19:52:21 +0300

    fix: Fix failing test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
+import { MOCK_RESPONSE } from "./App.mocks";
 
-jest.mock("./api");
+jest.mock("./api", () => ({
+  fakeApi: jest.fn(),
+}));
 
 describe("Test async", () => {
   test("should render users", async () => {
+    const { fakeApi } = require('./api');
+    fakeApi.mockReturnValue(MOCK_RESPONSE);
+
     render(<App />);
 
-    const userList = screen.getAllByRole("listitem");
-    expect(userList).toHaveLength(10);
+    const mockUser = await screen.findByText("Boris Johnson");
+    expect(mockUser).toBeInTheDocument();
   });
 });
